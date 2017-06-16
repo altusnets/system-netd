bc585e7b7da5d58e342a9fef9b28252e65a7bd1e
lubm1 <lubm1@lenovo.com>
2017-06-16 14:06:28 +0800

    IKANGEROW-6649: updated mtk solution to NW downlink packet drop issue.
    check POWERSAVE child chain as well as DOZABLE chain.
    
    Change-Id: I8a78d13af494675aef150695967c6588a5418194
    Reviewed-on: https://gerrit.mot.com/1012686
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Benmin Lu <lubm1@motorola.com>
    Submit-Approved: Jira Key

--- server/FirewallController.cpp
+++ server/FirewallController.cpp
@@ -166,16 +166,16 @@ int FirewallController::enableChildChains(ChildChain chain, bool enable) {
     }
 
     if (enable) {
-    //BEGIN Lenovo lubm1 2017-06-08 IKANGEROW-6264 VoLTE re-registration fails during idle.
+    //BEGIN Lenovo lubm1 2017-06-08 IKANGEROW-6264/IKANGEROW-6649 VoLTE re-registration fails during idle.
     //SIP msg from NW is dropped in DOZABLE chain,
     //so DUT is not able to continue re-registration upon re-reg timer expiration.
-        if(chain != DOZABLE)
+        if((chain != DOZABLE) && (chain != POWERSAVE))
             res |= attachChain(name, LOCAL_INPUT);
         res |= attachChain(name, LOCAL_OUTPUT);
     } else {
-        if(chain != DOZABLE)
+        if((chain != DOZABLE) && (chain != POWERSAVE))
             res |= detachChain(name, LOCAL_INPUT);
-    //END IKANGEROW-6264
+    //END IKANGEROW-6264/IKANGEROW-6649
         res |= detachChain(name, LOCAL_OUTPUT);
     }
     return res;
